c32f7609c7590d0ad7e7b6ca53a3dd9b490d5aa4
A_S_T_E_R_1_S_K_ <donhon05@gmail.com>
2021-07-21 11:53:52 +0900

    210721

---
+++ Chap1/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,11 @@
+/** Comment1
+ * Comment2
+ */
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        // sout으로 단축가능
+        System.out.println("example");
+    }
+}
---
+++ Chap1/HelloWorld/src/Variables.java
@@ -0,0 +1,23 @@
+public class Variables {
+    public static void main(String[] args) {
+        int age;
+        age = 27;
+
+        int num = 21;
+        age = 26;
+
+        // age = 22.6;
+
+        age = num;
+        age = age + 1;
+
+//        System.out.println("Hello I'm ");
+//        System.out.println(age);
+//        System.out.println(".");
+
+//         위의 것을 한줄로 사용하고 싶다면 아래처럼...
+        System.out.print("Hello I'm ");
+        System.out.print(age);
+        System.out.print(".");
+    }
+}
